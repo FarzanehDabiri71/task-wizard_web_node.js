bc43ea9e692b3254f1e33487e5e8be5a7c470e27
FarzanehDabiri71 <farzanehdabiri71@yahoo.com>
2025-05-28 12:29:38 +0330

    Enable URL-encoded form data parsing with express.urlencoded

--- app.js
+++ app.js
@@ -5,11 +5,15 @@ import express from "express";
 import "dotenv/config";
 
 import getRoutes from "./get-routes.js";
+import postRoutes from "./post-routes.js";
 
 const app = express();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: true }));
+
 app.use("/", getRoutes);
+app.use("/", postRoutes);
 app.listen(3000);
