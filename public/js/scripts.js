f9538860a8d73156774fe1a2ceb005583a0cc5dc
FarzanehDabiri71 <farzanehdabiri71@yahoo.com>
2025-05-30 22:45:32 +0330

    Handle task deletion with confirmation and UI update

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -9,7 +9,24 @@ list.addEventListener("click", async (event) => {
       const response = await axios.post("/toggle-task", { id });
 
       if (response.data) {
-        location.reload();
+        const badge = target.parentElement.querySelector(".badge");
+        const isCompleted = badge.classList.contains("bg-success");
+
+        if (isCompleted) {
+          badge.classList.remove("bg-success");
+          badge.classList.add("bg-secondary");
+          badge.innerText = "In progress";
+
+          target.classList.remove("btn-secondary");
+          target.classList.add("btn-success");
+        } else {
+          badge.classList.remove("bg-secondary");
+          badge.classList.add("bg-success");
+          badge.innerText = "Completed";
+
+          target.classList.remove("btn-success");
+          target.classList.add("btn-secondary");
+        }
       } else {
         alert(response.data);
       }
@@ -23,7 +40,7 @@ list.addEventListener("click", async (event) => {
       try {
         const response = await axios.post("/edit-task", { id, title: answer });
         if (response.data === true) {
-          location.reload();
+          target.parentElement.querySelector("label").innerText = answer;
         } else {
           alert(response.data);
         }
@@ -33,6 +50,20 @@ list.addEventListener("click", async (event) => {
     } else if (answer) {
       // cancel رو شامل نمیشه
       alert("Please enter at least 3 characters.");
+    }
+  } else if (target.classList.contains("delete-btn")) {
+    if (confirm("Are you sure you want to delete this task?")) {
+      try {
+        const response = await axios.post("/delete-task", { id });
+
+        if (response.data) {
+          target.parentElement.remove();
+        } else {
+          alert(response.data);
+        }
+      } catch (error) {
+        alert(e.response.data);
+      }
     }
   }
 });
